65ccf41dabf36f5cafdae1d0b7b2c19124ac8e15
Jesus Escalona <jesusescalona@me.com>
2019-04-17 22:07:06 -0400

    Done

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let x = document.getElementById("myTopnav");
+function myFunction() {
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
